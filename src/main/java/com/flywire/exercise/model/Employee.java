6dbb22111967b6b0dfdbcf639b1c34ade1c8307c
dhrubot <dhrubo.dh@gmail.com>
2024-10-22 16:52:13 -0400

    added lombok to the deps and created employee model

---
+++ src/main/java/com/flywire/exercise/model/Employee.java
@@ -0,0 +1,18 @@
+package com.flywire.exercise.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+public class Employee {
+    private Long id;
+    private String name;
+    private String position;
+    private LocalDate hireDate;
+    private boolean active;
+    private List<Long> directReports;
+}
