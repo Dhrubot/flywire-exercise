9e7c598b6fd4e8ae9eac6367246baba3c4a7ed52
dhrubot <dhrubo.dh@gmail.com>
2024-10-23 14:39:00 -0400

    fixing main

--- client/src/types/employees.type.ts
+++
@@ -1,9 +0,0 @@
-export type TEmployeeTypes = {
-    id: number;
-    name: string;
-    position: string;
-    hireDate: string;
-    active: boolean;
-    parsedHireDate: string;
-    directReports: string[];
-  };
